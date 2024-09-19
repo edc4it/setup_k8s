c0d7fe446cada53a2a67495685f4400471ffacbe
Raphael Parree <rparree@edc4it.com>
2024-09-19 14:50:02 +0200

    fix: corrected the test to load zshrc

--- test.sh
+++ test.sh
@@ -1,14 +1,17 @@
 
 #! /bin/bash
 
-# --destroy=never
+# test
+# test --destroy=never
+# verify
+
 podman run --rm \
   -it \
   -v $(pwd):/project \
   -v "${MOLECULE_DOCKER_SOCK:-/var/run/docker.sock}:/var/run/docker.sock:ro" \
   -w /project \
   registry.edc4it.com/cwdecks/delivery-management/awx/ansible-molecule  \
-  molecule test   \
+  molecule    \
   "$@"
 
 
